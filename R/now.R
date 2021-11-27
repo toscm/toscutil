cfe8862337be0b19e4e2cc22a76cef6c905a7172
Tobias Schmidt <tobias2.schmidt@ukr.de>
2021-11-27 19:38:17 +0100

    Exported now function

--- R/now.R
+++ R/now.R
@@ -1,5 +1,6 @@
 #' @title now
-#' @description return current system time as string of the form YYYY-MM-DD  hh:mm:ss
+#' @description return current system time as string of the form YYYY-MM-DD hh:mm:ss
 #' @examples
 #' now() # "2021-11-27, 19:19:31"
+#' @export 
 now <- function() format(Sys.time(), '%Y-%m-%d, %H:%M:%S')
