a2d8c3468326a76a78cf31b636bdf3971bef5cd6
Tobias Schmidt <tobias2.schmidt@ukr.de>
2023-07-19 23:33:50 +0200

    Next try

---
+++ tests/testthat/test-split_docstring.R
@@ -0,0 +1,31 @@
+test_that("update_docstring works", {
+    uri <- system.file("testfiles/funcs.R", package = "toscutil")
+    content <- readLines(uri)
+    docstring <- get_docstring(content, "f2")
+    dfs <- split_docstring(docstring)
+    head_expected <- c(
+      header = "",
+      title = "#' @title Sum of Vector Elements\n",
+      description = "#' @description f3 returns the sum of all the values present in its arguments.\n"
+    )
+    param_expected <- c(
+      b = "#' @param b TODO\n",
+      a = "#' @param a Already documented\n",
+      z = "#' @param z Multiline description\n#'\n#' of parameter z.\n#'\n",
+      ... = "#' @param ... unused\n"
+    )
+    tail_expected <- c(
+      export = "#' @export\n",
+      details = "#' @details This is a generic function: methods can be defined for it directly\n#' or via the Summary group generic. For this to work properly, the arguments\n#' ... should be unnamed, and dispatch is on the first argument."
+    )
+    message("\nHEAD:")
+    message(dfs$head)
+    message("\nPARAM:")
+    message(dfs$param)
+    message("\nTAIL:")
+    message(dfs$tail)
+    message("")
+    testthat::expect_equal(dfs$head, head_expected)
+    testthat::expect_equal(dfs$param, param_expected)
+    testthat::expect_equal(dfs$tail, tail_expected)
+})
--- tests/testthat/test-update_docstring.R
+++ tests/testthat/test-update_docstring.R
@@ -1,24 +1,6 @@
 test_that("update_docstring works", {
     uri <- system.file("testfiles/funcs.R", package = "toscutil")
     content <- readLines(uri)
-    local({ # Content of uri looks like this:
-      #' @title Sum of Vector Elements
-      #' @description f3 returns the sum of all the values present in its arguments.
-      #' @param b TODO
-      #' @param a Already documented
-      #' @export
-      #' @param z Multiline description
-      #'
-      #' of parameter z.
-      #'
-      #' @param ... unused
-      #' @details This is a generic function: methods can be defined for it directly
-      #' or via the Summary group generic. For this to work properly, the arguments
-      #' ... should be unnamed, and dispatch is on the first argument.
-      f2 <- function(a, b, x = 1, ...) {
-        cat("f3")
-      }
-    })
     updated_docstring <- update_docstring(uri, "f2")
     expected_docstring <- paste(
       "#' @title Sum of Vector Elements",
